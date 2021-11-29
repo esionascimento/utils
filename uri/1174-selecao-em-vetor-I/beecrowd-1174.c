2c2058c52de914589c7b722c3c5dce49e1776f4c
esionascimento <esio.392@protonmail.com>
2021-11-29 11:12:14 -0400

    1174 concluded

---
+++ uri/1174-selecao-em-vetor-I/beecrowd-1174.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+  int i;
+  float v[100], x;
+  for (i = 0; i < 100; i++) {
+    scanf("%f", &v[i]);
+  }
+  for (i = 0; i < 100; i++) {
+    if (v[i] <= 10) {
+      printf("A[%d] = %.1f\n", i, v[i]);
+    }
+  }
+  return 0;
+}
